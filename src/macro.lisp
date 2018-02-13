b5d668dfd439d3155965250192722673686e9beb
Tim Menzies <tim.menzies@gmail.com>
2018-02-12 19:12:32 -0500

    better readme

--- src/macro.lisp
+++ src/macro.lisp
@@ -52,6 +52,23 @@
   `(defclass ,x (,parent)
      ,(loop for (slot form) in slots collect (defslot slot x form))))
 
+(defmethod public-slot-names ((it thing))
+    (remove-if 
+      #'(lambda (x)
+          (and (symbolp x) 
+               (equal (elt (symbol-name x) 0) #\_)))
+      (mapcar 
+        #'slot-definition-name
+        (class-slots (class-of it)))))
+
+(defmethod print-object ((it thing) out)
+   (let ((lst (mapcar 
+                #'(lambda (s)
+                    (list s (slot-value it s)))
+                (sort (public-slot-names it)
+                      #'string<))))
+    (format out "~a" (cons (class-name (class-of it)) lst))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro ? (obj first-slot &rest more-slots)
   "From https://goo.gl/dqnmvH:"
--- src/macrook.lisp
+++ src/macrook.lisp
@@ -4,8 +4,9 @@
   "macro")
 
 (deftest _aif ()
-  (if (- 10 5)
-    (print `(it ,it))))
+  "anaphoric if"
+  (aif (- 10 5)
+    (test 5 it)))
 
 (deftest _doitems ()
   "seeking association list"
@@ -15,4 +16,51 @@
           (doitems (one n lst out)
             (push (cons n one) out)))))
 
-#-tests (tests)
+(deftest _doh ()
+  "walking a hash table"
+  (let ((n 0)
+        (h (make-hash-table)))
+    (setf 
+      (gethash 'a h) 10
+      (gethash 'b h) 20
+      (gethash 'c h) 30)
+    (test 60
+          (doh (k v h n)
+            (print `(k ,k v ,v))
+            (incf n v))))
+)
+
+(deftest _while()
+  "whiling away the hours"
+  (let ((n 10)
+        (m 0))
+    (while (> (decf n) 0)
+      (incf m n))
+    (test 45 m)))
+
+(deftest _until()
+  "whiling away the hours"
+  (let ((n 10)
+        (m 0))
+    (until (zerop (decf n))
+      (incf m n))
+    (test 45 m)))
+
+(deftest _o ()
+  "doing a print demo"
+  (let ((l 5)
+        (m 10)
+        (n 20))
+    (o l m n)))
+
+(defthing stuff thing (a 1) (b 2) (_secret "tim"))
+
+(defthing our stuff (d 10) (_hush 30))
+
+(deftest _thing()
+  "things"
+  (let ((it (make-instance 'our :b 23)))
+    (print it)
+    (test 30 (? it _hush))))
+
+(tests)
